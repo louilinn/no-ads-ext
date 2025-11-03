a00c590a89b2f636f672d28a632074b01f6fc1c4
daniel <namatoj@gmail.com>
2025-11-03 22:31:38 +0100

    Add and process the easylist (naively)

--- src/no-ads-background.js
+++ src/no-ads-background.js
@@ -3,11 +3,12 @@ const badUrls = [
     ]
 
 function detectBadUrl(requestDetails) {
-  console.log(requestDetails)
   for (const badUrl of badUrls) {
     const isBad = badUrl.test(requestDetails.url)
 
     if (isBad) {
+      console.log(requestDetails)
+      console.log("active rule:", badUrl)
       browser.tabs.sendMessage(
         requestDetails.tabId,
         "bad"
@@ -24,6 +25,33 @@ function detectBadUrl(requestDetails) {
     }
   }
 }
+
+
+async function loadUrls() {
+  const url = browser.runtime.getURL('/resources/easylist.txt');
+  const response = await fetch(url);
+  const text = await response.text();
+  for (const line of text.split('\n')) {
+    if(line[0] === '!' || line.substring(0, 2) === "@@" || line.includes('#')) continue
+
+    let simpleRule = line.split("$")[0]
+    simpleRule = simpleRule.replaceAll('.', '\\.')
+    simpleRule = simpleRule.replaceAll('/', '\\/')
+    simpleRule = simpleRule.replaceAll('?', '\\?')
+    simpleRule = simpleRule.replaceAll('[', '\\[')
+    simpleRule = simpleRule.replaceAll(']', '\\]')
+    simpleRule = simpleRule.replaceAll('||', '')
+    simpleRule = simpleRule.replaceAll('|', '')
+    simpleRule = simpleRule.replaceAll('^', '')
+
+
+    console.log(line, simpleRule)
+    if (simpleRule.length === 0) continue
+    badUrls.push(new RegExp(simpleRule))
+  }
+}
+
+loadUrls();
 
 browser.webRequest.onBeforeRequest.addListener(detectBadUrl, {
   urls: ["<all_urls>"],
